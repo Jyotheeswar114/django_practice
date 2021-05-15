c4abfd56088a7a4d704c9c6b8fbaeb54991c6b4d
jyotheeswar114 <jyotheeswarganne60@gmail.com>
2021-05-15 15:00:41 +0530

    Added multiple ignores

--- django_practice/settings.py
+++ django_practice/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_practice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_practice',
+        'USER': 'root',
+        'PASSWORD': 'jyo12345',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
